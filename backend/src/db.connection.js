986d60f6cdd14d2c3f3e5ffbe1f5a67c8f62666a
Giovanni Robira <giovanni.robira@accenture.com>
2019-08-14 14:19:03 -0300

    Mongoose events log

--- backend/src/db.connection.js
+++ backend/src/db.connection.js
@@ -5,14 +5,23 @@ module.exports = (() => {
     .connect(
       `mongodb+srv://${process.env.MONGO_USER}:${
         process.env.MONGO_PASS
-      }@remotemongo-wtm1e.mongodb.net/omnistack8?retryWrites=true&w=majority`,
-      { useNewUrlParser: true },
+      }@remotemongo-wtm1e.mongodb.net/tindev?retryWrites=true&w=majority`,
+      { useCreateIndex: true, useNewUrlParser: true },
     )
-    .then(() => {
-      console.log('Connected to remote database.');
-    })
     .catch(err => {
       console.log(err);
       console.error('Failed to connected with remote database');
     });
+
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to remote database.');
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('Disconnected from remote database.');
+  });
+
+  mongoose.connection.on('reconnected', () => {
+    console.log('reconnected to remote database.');
+  });
 })();
