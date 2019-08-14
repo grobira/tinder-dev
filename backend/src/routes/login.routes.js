80b69367bd77d485e140654cf8557589334d5185
Giovanni Robira <giovanni.robira@accenture.com>
2019-08-14 14:22:37 -0300

    Login route

--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -1,7 +1,6 @@
 const routes = require('express').Router();
 const LoginController = require('../controllers/login.controller');
 
-routes.post('/login', LoginController.store);
-routes.post('/login2', LoginController.login);
+routes.post('/login', LoginController.login);
 
 module.exports = routes;
