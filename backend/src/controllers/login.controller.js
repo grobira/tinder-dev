9ad43559f79b1cf69f973aeef34a3b5af1edd7a7
Giovanni Robira <giovanni.robira@accenture.com>
2019-08-14 14:24:56 -0300

    Login refactor - Jwt auth - password encrypt - error handler

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -1,53 +1,70 @@
-const axios = require('axios');
-const DevModel = require('../models/dev.model');
+const { createJwtToken } = require('../utils/auth');
 const UserModel = require('../models/user.model');
+const DevModel = require('../models/dev.model');
 const { DevDto } = require('../models/dev.dto');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+const {
+  WrongPasswordException,
+  MissingParametersException,
+} = require('../utils/exceptions');
 
 module.exports = {
-  store: async (req, res) => {
-    let { username } = req.body;
-    if (!username) {
-      return res.status(400).send({
-        error: '400 - Bad Parameters',
-        message: 'Missing username on body',
-      });
-    }
-    username = username.trim();
-    let dev = await DevModel.findOne({ username });
-
-    if (!dev) {
-      const response = await axios.get(
-        `https://api.github.com/users/${username}`,
-      );
-      const { name, bio, avatar_url: avatar } = response.data;
-
-      dev = await DevModel.create({ name, username, avatar, bio });
-    }
-
-    return res.json(DevDto(dev));
-  },
-  login: async (req, res) => {
-    const { login, password } = req.locals;
+  login: async (req, res, next) => {
+    const { login, password } = req.body;
 
     if (!login || !password) {
-      return res.status(400).send({
-        error: '400 - Bad Parameters',
-        message: 'Missing login or password',
-      });
+      return next(
+        new MissingParametersException({
+          details: 'Missing login or password',
+        }),
+      );
     }
 
     if (validatePassword(password)) {
       const exist = await UserModel.findOne({ login });
 
       if (exist) {
+        try {
+          await comparePassword(password, exist.password);
+          const token = createJwtToken({ login, password });
+          return res.json({ result: 'User logged', token });
+        } catch (err) {
+          return next(err);
+        }
       } else {
-        const newUser = await UserModel.create({ login, password });
-        return res.redirect('/login');
+        createDev(login);
+        await UserModel.create({ login, password });
+        const token = createJwtToken({ login, password });
+        return res.json({ result: 'User successfully added', token });
       }
     }
   },
 };
 
-function validatePassword(password) {
+const validatePassword = password => {
   return true;
-}
+};
+
+const createDev = async username => {
+  let dev = await DevModel.findOne({ username });
+
+  if (!dev) {
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`,
+    );
+    const { name, bio, avatar_url: avatar } = response.data;
+
+    dev = await DevModel.create({ name, username, avatar, bio });
+  }
+
+  return DevDto(dev);
+};
+
+const comparePassword = (candidatePassword, userPassword) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, userPassword).then(isMatch => {
+      isMatch ? resolve() : reject(new WrongPasswordException());
+    });
+  });
+};
